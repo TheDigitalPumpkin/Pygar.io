4f162f33813f0030c86089e28de64300bbb13db1
Bruno Bacelar <39466847+TheDigitalPumpkin@users.noreply.github.com>
2019-05-14 23:41:17 -0300

    Add files via upload

--- entities/enemy.py
+++ entities/enemy.py
@@ -13,7 +13,8 @@ class Enemy(Player):
         self.image.fill(PURPLE)
         self.rect = self.image.get_rect()
         self.size = 8
-        self.directions = [-1, 1, 0, 1, -1]
+        self.dist_from_player = 10000
+        self.directions = [-2, 0, 2]
 
         self.x = random.randrange(0, 1300)
         self.y = random.randrange(0, 760)
@@ -24,6 +25,7 @@ class Enemy(Player):
 
     def distance_from_player(self, player):
         dist = math.sqrt((self.x - player.get_x()) ** 2 + (self.y - player.get_y()) ** 2)
+        self.dist_from_player = dist
         try:
             x = (player.get_x() - self.x) / dist
             y = (player.get_y() - self.y) / dist
@@ -31,13 +33,27 @@ class Enemy(Player):
             return False
         return (x, y)
 
-    def update(self, player):
+    def get_distance_from_food(self, food_list):
+        dist_food = {}
+        for i in range(0, len(food_list)):
+            dist = math.sqrt((self.x - food_list[i].get_x()) ** 2 + (self.y - food_list[i].get_y()) ** 2)
+            dist_food[dist] = food_list[i]
+
+        sorted(dist_food)
+        return dist_food
+
+    def update(self, player, food_dist_list):
         new_position = self.distance_from_player(player)
-        if new_position != 0:
+
+        ## TODO: Fix crashing when dealing with multiple AI
+        if new_position != 0 and self.dist_from_player <= 200:
             self.x, self.y = (self.x + new_position[0] / 2, self.y + new_position[1] / 2)
         else:
-            self.x = self.directions[random.randrange(0, 4)]
-            self.y = self.directions[random.randrange(0, 4)]
+            nearest_food = food_dist_list[list(food_dist_list.keys())[0]]
+            relative_x, relative_y = nearest_food.get_x() - self.x, nearest_food.get_y() - self.y
+            angle = math.atan2(relative_y, relative_x)
+            self.x += math.cos(angle)
+            self.y += math.sin(angle)
 
     def increase_size(self, size):
         self.size += size / 4
